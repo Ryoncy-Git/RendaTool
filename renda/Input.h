5b91e97756933281cb8fd9a1352c76a6361f10ce
Ryoncy-Git <ryonryon5329@i.softbank.jp>
2025-10-21 13:50:30 +0900

    releaseブランチを確保

--- renda/Input.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include <wchar.h>
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
